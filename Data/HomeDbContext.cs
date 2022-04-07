9221c3513a223d17cf47e2b2680d8ccca88d11c6
fotini <f.batzoni@gmail.com>
2022-04-07 12:23:27 +0300

    Add all files

---
+++ Data/HomeDbContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using E_stateAgency.Models;
+
+namespace E_stateAgency.Data
+{
+    public class HomeDbContext : DbContext
+    {
+        public HomeDbContext (DbContextOptions<HomeDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<E_stateAgency.Models.House> House { get; set; }
+    }
+}
